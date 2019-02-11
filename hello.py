266b613c5715cf181121d38ce8cfd475c3afcab7
Sanela Becovic <sanela.becovic@udg.edu.me>
2019-02-11 13:52:14 +0100

    Dodaj ime i prezime

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
 print("Hello World")
+print("Moje ime je Sanela")
+
+def greet(ime, prezime):
+
+    tekst_koji_pisemo="Hello there" + " " +  ime + " " + " " + prezime + "!"
+    print(tekst_koji_pisemo)
+
+greet("Sanela" , "Becovic")
+
+
+
